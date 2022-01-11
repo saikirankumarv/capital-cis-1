732a0016ed582c3cca14bfb0a18837f107d5a958
Neo2505 <84080534+Neo2505@users.noreply.github.com>
2022-01-11 22:50:54 +0530

    Update App.java

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "change is from webhook" );
+        System.out.println( "change is from webhook2" );
     }
 }
