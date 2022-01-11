fabc8ee4141e98488bb87467feaefa4517176a56
Neo2505 <84080534+Neo2505@users.noreply.github.com>
2022-01-11 13:29:57 +0530

    github webhook change

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "This is poll scm 2 change..." );
+        System.out.println( "change is from github webhook" );
     }
 }
