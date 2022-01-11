90f29d99e3bd7fec650476d4e5fd9fbe38e010fc
Neo2505 <84080534+Neo2505@users.noreply.github.com>
2022-01-11 11:56:38 +0530

    updated

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "This is poll scm change..." );
+        System.out.println( "This is poll scm 2 change..." );
     }
 }
